5964b80eaa4b5a41acddc46462b3c820f32b7c45
daey <kodhyoung__@naver.com>
2025-02-18 19:04:21 +0900

    25. 02. 18. commit

---
+++ baekjoon/src/step01/BOJ_1008.java
@@ -0,0 +1,19 @@
+package step01;
+
+// A/b, https://www.acmicpc.net/problem/1008
+
+import java.util.*;
+
+public class BOJ_1008 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt(), b = sc.nextInt();
+
+		System.out.println((double)a/b);
+		
+	}
+
+}
