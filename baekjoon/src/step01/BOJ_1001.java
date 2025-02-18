b19ea8d0cec7249c1d7da37f05881b7c5a396207
daey <kodhyoung__@naver.com>
2025-02-18 18:46:51 +0900

    25. 02. 18. commit

---
+++ baekjoon/src/step01/BOJ_1001.java
@@ -0,0 +1,19 @@
+package step01;
+
+// A-B, https://www.acmicpc.net/problem/1001
+
+import java.util.*;
+
+public class BOJ_1001 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt(), b = sc.nextInt();
+
+		System.out.println(a-b);
+
+	}
+
+}
---
+++ baekjoon/src/step01/BOJ_2557.java
@@ -0,0 +1,13 @@
+package step01;
+
+// Hello World, https://www.acmicpc.net/problem/2557
+
+public class BOJ_2557 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+
+}
